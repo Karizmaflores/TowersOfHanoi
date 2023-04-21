3462c301844951e25a7f65b288a9cea8e6b59f30
Karizmaflores <flores_karizma@yahoo.com>
2023-04-20 22:01:36 -0500

    added code comments to index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-// * This js file is incomplete. It will log to the console the elements you click
-    // call another function and set stone. You will have to work through the logic
-    // of the game as you know it from building it in the terminal. Work through the
-    // puzzle slowly, stepping through the flow of logic, and making the game work.
-    // Have fun!!
-// * First run the program in your browser with live server and double-click on the row you'd like to select an element from.
-// * Why are you get a warning in your console? Fix it.
-// * Delete these comment lines!
 
 let stone = null
 
-// this function is called when a row is clicked. 
-// Open your inspector tool to see what is being captured and can be used.
+// This function console logs that player clicked a row
 const selectRow = (row) => {
   const currentRow = row.getAttribute("data-row")
   
@@ -18,7 +9,7 @@ const selectRow = (row) => {
   console.log("Here is the stone's id: ", row.id)
   console.log("Here is the stone's data-size: ", currentRow)
 
- 
+ // This function will run pickUpStone is hand is empty or dropStone from player's hand and checkWin
   if(!stone){
     console.log("marker");
     pickUpStone(row.id);
@@ -29,8 +20,7 @@ const selectRow = (row) => {
   
 } 
 
-// this function can be called to get the last stone in the stack
-// but there might be something wrong with it...
+// This function picks up the stone on the top of the row
 const pickUpStone = (rowID) => {
   const selectedRow = document.getElementById(`${rowID}`);
   console.log(rowID);
@@ -42,9 +32,7 @@ const pickUpStone = (rowID) => {
   console.log(stone)
 }
 
-// You could use this function to drop the stone but you'll need to toggle between pickUpStone & dropStone
-// Once you figure that out you'll need to figure out if its a legal move...
-// Something like: if(!stone){pickupStone} else{dropStone}
+// This function stores and drops the stone if the move is legal
 
 const dropStone = (rowID) => {
   const selectedRow = document.getElementById(`${rowID}`);
@@ -70,6 +58,7 @@ const dropStone = (rowID) => {
   
 }
 
+// This function checks for player win based on item ids of the stones
 const checkWin = (row) => {
 
   let one = document.getElementById("1");
